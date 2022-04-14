e4d8cbac100da85f7aa1f4fa8b84eb2ab919c79a
tolunayelibol <100027467+tolunayelibol@users.noreply.github.com>
2022-04-14 03:33:11 +0300

    Update server.cpp

--- TCPServer/server.cpp
+++ TCPServer/server.cpp
@@ -8,11 +8,11 @@ Server::Server(QObject *parent)
     connect (server,SIGNAL(newConnection()),this,SLOT(newConnection()));
     if (!server->listen(QHostAddress::Any,1234))
     {
-        qDebug() <<"Server could not start!";
+        qDebug() <<"Server henuz baslamadi!";
     }
     else
     {
-        qDebug() <<"Server started";
+        qDebug() <<"Server Basladi";
 
     }
 
@@ -21,7 +21,7 @@ Server::Server(QObject *parent)
  {
  QTcpSocket *socket = server->nextPendingConnection();
 
- socket->write("hello client\r\n");
+ socket->write("hosgeldiniz\r\n");
  socket->flush();
  socket->waitForBytesWritten(3000);
  socket->close();
